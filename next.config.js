93311731485183735b580e6e3e6a6c5fd9f44c2e
Avnish <avnish.chhabra@createbytes.com>
2021-08-15 18:56:31 +0530

    3rd try

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     images: {
         domains: ['links.papareact.com']
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
+            }
+        }
+
+        return config;
     }
 }
