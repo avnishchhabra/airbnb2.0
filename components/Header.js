9e514988d027b5831d751e917b59da5aadc88163
Avnish <avnish.chhabra@createbytes.com>
2021-08-15 19:07:05 +0530

    err solved

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,6 @@ import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from 'react-date-range';
 import { useRouter } from "next/dist/client/router";
-import { route } from "next/dist/next-server/server/router";
 
 function Header({placeholder}) {
   const [searchInput, setSearchInput] = useState("");
